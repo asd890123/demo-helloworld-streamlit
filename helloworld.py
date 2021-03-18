91137b1665f92ebb95fb5e005a3beb50bb6eb4ef
asd890123 <asd890123@sina.cn>
2021-03-18 18:12:36 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -8,36 +8,11 @@ x = st.slider("x")
 y = x + 3
 y
 
-'''
-@st.cache
-def load_data():
-    df = pd.read_csv("crime_data_20200508.csv")
-    #df = df[['EVENT_TYPE', 'CREATE_TIME', 'COUNTY', 'LAT', 'LON']]
-    crime,locname,incidentdatetime,publicadress,agency,accuracy
-    #df.columns = ['event_type', 'time', 'county', 'lat', 'lon']
-    return df
 
-df = load_data()
+# Reuse this data across runs!
+read_and_cache_csv = st.cache(pd.read_csv)
 
-st.table(df.head())
-
-event_list = df["event_type"].unique()
-
-event_type = st.sidebar.selectbox(
-    "Which kind of event do you want to explore?",
-    event_list
-)
-
-county_list = df["county"].unique()
-
-county_name = st.sidebar.selectbox(
-    "Which county?",
-    county_list
-) 
-
-part_df = df[(df["event_type"]==event_type) & (df['county']==county_name)]
-
-st.write(f"根据你的筛选，数据包含{len(part_df)}行")
-
-st.map(part_df)
-'''
+BUCKET = "https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
+data = read_and_cache_csv(BUCKET + "labels.csv.gz", nrows=1000)
+desired_label = st.selectbox('Filter to:', ['car', 'truck'])
+st.write(data[data.label == desired_label])
