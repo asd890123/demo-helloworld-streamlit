0c0c7dd3734035702d4dabb39c732af43a28ed7d
asd890123 <asd890123@sina.cn>
2021-06-13 11:15:13 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -5,14 +5,16 @@ import pandas as pd
 st.title("my first app")
 
 x = st.slider("x")
-y = x + 3
+y = 2 * x + 5
 y
 
 
 # Reuse this data across runs!
+'''
 read_and_cache_csv = st.cache(pd.read_csv)
 
 BUCKET = "https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
 data = read_and_cache_csv(BUCKET + "labels.csv.gz", nrows=1000)
 desired_label = st.selectbox('Filter to:', ['car', 'truck'])
 st.write(data[data.label == desired_label])
+'''
